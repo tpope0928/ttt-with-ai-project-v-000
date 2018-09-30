1d462d6b44149d392ea9dd6ccf352ca7ee66b3cc
Thomas Pope <tpope0928@gmail.com>
2018-09-30 21:01:08 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,29 +1,40 @@
 module Players
   class Computer < Player
-      attr_accessor :board, :cells
+    def move(board)
+      move = nil
+      if !board.taken?(5)
+        move = "5"
 
-      def move(board)
-         if board.cells[0] == " " && board.cells[4] == " "
-           "1"
-         elsif board.cells[4] == " "
-           "5"
-         elsif board.cells[2] == " "
-           "3"
-         elsif board.cells[6] == " "
-           "7"
-         elsif board.cells[8] == " "
-           "9"
-         elsif board.cells[1] == " "
-           "2"
-         elsif board.cells[3] == " "
-           "4"
-         elsif board.cells[5] == " "
-           "6"
-         elsif board.cells[7] == " "
-           "8"
-         end
-       end
+      
+      elsif board.turn_count == 1
+        move = "1"
 
+      
+      elsif board.turn_count == 2
+        move = [1, 3, 7, 9].detect{|i| !board.taken?(i)}.to_s
+
+      
+      elsif board.turn_count == 3 && (board.position(1) == board.position(9) || board.position(3) == board.position(7))
+        move = "2"
+
+      
+      else
+        Game::WIN_COMBINATIONS.detect do |cmb|
+
+         
+          if cmb.select{|i| board.position(i+1) == token}.size == 2 && cmb.any?{|i| board.position(i+1) == " "}
+            move = cmb.select{|i| !board.taken?(i+1)}.first.to_i.+(1).to_s
+
+          
+          elsif cmb.select{|i| board.position(i+1) != " " && board.position(i+1) != token}.size == 2 && cmb.any?{|i| board.position(i+1) == " "}
+            move = cmb.select{|i| !board.taken?(i+1)}.first.to_i.+(1).to_s
+          end
+        end
+
+       
+        move = [1, 3, 7, 9, 2, 4, 6, 8].detect{|i| !board.taken?(i)}.to_s if move == nil
+      end
+      move
+    end
   end
-
 end
