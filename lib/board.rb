c1f9796c66ea916ebcb012d6bb39fe4521395edf
Thomas Pope <tpope0928@gmail.com>
2018-09-30 18:35:04 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -14,7 +14,8 @@ class Board
     puts "-----------"
     puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
     puts "-----------"
-    puts " #{cells[6]} | #{cells[7]} | #{cells[8  end
+    puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
+  end
   
   
 end
