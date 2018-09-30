b32d8926daac7d7af0c0bb886bcf8f8c346a0dd9
Thomas Pope <tpope0928@gmail.com>
2018-09-30 20:33:43 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -24,7 +24,7 @@ class Game
   
   def won?
     WIN_COMBINATIONS.detect do |winner|
-      @board.cells[winner[0]] == @board.cells[winner[1]] && 
+      @board.cells[winner[0]] == @board.cells[winner[1]] && @board.cells[winner]
     end  
   end
   
